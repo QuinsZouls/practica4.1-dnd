54f71a839e35e911aecad638530c46daa1670915
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-09-22 13:44:28 -0500

    fix: remove target dictionary

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-
-
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -19,20 +17,9 @@ driver.maximize_window()
 
 time.sleep(2)
 
-answers = {
-    '//*[@id="box1"]': '//*[@id="box101"]',
-    '//*[@id="box2"]': '//*[@id="box102"]',
-    '//*[@id="box3"]': '//*[@id="box103"]',
-    '//*[@id="box4"]': '//*[@id="box104"]',
-    '//*[@id="box5"]': '//*[@id="box105"]',
-    '//*[@id="box6"]': '//*[@id="box106"]',
-    '//*[@id="box7"]': '//*[@id="box107"]',
-}
-
-for x, y in answers.items():
-    src = driver.find_element(By.XPATH, x)
-    dest = driver.find_element(By.XPATH, y)
+for i in range(1, 8):
+    src = driver.find_element(By.ID, "box%i" % i)
+    dest = driver.find_element(By.ID, "box10%i" % i)
     action.drag_and_drop(src, dest).perform()
     time.sleep(1)
-
 driver.quit()
